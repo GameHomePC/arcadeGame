3f168f3f318fc4fbaa008230ef1d89a27686527f
Eugene Alexeenko <yauheni.aliakseyenko@neklo.com>
2014-12-07 14:42:32 +0300

    phaser init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* height and width canvas */
+var widthCanvas = 800,
+    heightCanvas = 600;
+
+/* init Phaser Object */
+var game = new Phaser.Game(widthCanvas, heightCanvas, Phaser.WEBGL, 'window', {
+    preload: preload,
+    create: create,
+    update: update
+});
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+    
+}
